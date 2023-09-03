1ab8d14f9ee9844f3800fecab4d325f8bfe937a9
Ho Hong Meng <37105274+melvinho77@users.noreply.github.com>
2023-09-03 15:49:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-customhost = "employee.cgtpcksgf7rv.us-east-1.rds.amazonaws.com"
+customhost = "employee.cbffvuek6hel.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "hohongmeng-bucket"
-customregion = "us-east-1"
-
+custombucket = "lokekoklam-employee1"
+customregion = "us-east-1"
